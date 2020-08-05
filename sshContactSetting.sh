639754c693e3db23be85982fbb4716d4e6496c9a
logan <420885426@qq.com>
2020-08-05 15:12:55 +0800

    change sshSettings file

--- sshContactSetting.sh
+++ sshContactSetting.sh
@@ -4,7 +4,7 @@ reference url:https://www.jianshu.com/p/3e4533b5034f
 """
 echo export TMOUT=1000000 >> /root/.bash_profile
 cat /root/.bash_profile
-source .bash_profile
+source /root/.bash_profile
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config_bak
 echo ClientAliveInterval=60 >> /etc/ssh/sshd_config
 service sshd restart
